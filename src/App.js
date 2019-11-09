72a034dc37ebfbc7f0b355f7235a0574fcfe8804
Casey Carroll <caseydean8@gmail.com>
2019-11-09 11:30:00 -0500

    added delete button that uses filter method to remove list item

--- src/App.js
+++ src/App.js
@@ -7,27 +7,50 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1, 
-        title: 'Email recruiter',
+        id: 1,
+        title: "Email recruiter",
         completed: false
       },
       {
-        id: 2, 
-        title: 'Message Chris',
+        id: 2,
+        title: "Message Chris",
         completed: false
       },
       {
-        id: 3, 
-        title: 'Make flashcards',
+        id: 3,
+        title: "Make flashcards",
         completed: true
       }
     ]
-  }
+  };
+
+  // Toggle complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
   render() {
-    console.log(this.state.todos)
+    console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 export class TodoItem extends Component {
   getStyle = () => {
     return {
-      background: '#f4f4f4',
-      padding: '10px',
-      borderBottom: '1px #ccc dotted',
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
       textDecoration: this.props.todo.completed ? "line-through" : "none"
     };
     // if(this.props.todo.completed) {
@@ -20,18 +20,24 @@ export class TodoItem extends Component {
     // }
   };
 
-  markComplete = (e) => {
-    console.log(this.props)
-  }
+  markComplete = e => {
+    console.log(this.props);
+  };
 
   render() {
+    const { id, title } = this.props.todo;
     return (
       // inline style style={{backgroundColor: "#f4f4f4"}}
       // const style style={itemStyle}
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.props.markComplete} /> {' '}
-        {this.props.todo.title}</p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+        </p>
       </div>
     );
   }
@@ -41,6 +47,16 @@ export class TodoItem extends Component {
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
 };
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: '5px 8px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
 
 // see line 26 (approx)
 // const itemStyle = {
